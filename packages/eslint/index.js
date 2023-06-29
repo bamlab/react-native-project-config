a4fac67855682119905d6426edf91c980750443a
Alexis Delage <alexisd@bam.tech>
2023-06-29 14:41:13 +0200

    feat: ignore default config files for analysis

--- packages/eslint/index.js
+++ packages/eslint/index.js
@@ -8,6 +8,17 @@ module.exports = defineConfig({
     node: true,
     "react-native/react-native": true,
   },
+  ignorePatterns: [
+    ".cache", // tsc/eslint/metro cache
+    ".expo-shared",
+    ".expo",
+    ".yarn", // yarn 3
+    "android", // react-native
+    "ios", // react-native
+    "coverage", // jest
+    "dist", // expo updates
+    "node_modules",
+  ],
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
