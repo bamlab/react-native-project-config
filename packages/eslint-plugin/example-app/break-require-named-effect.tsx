ed58155ad43f86507662f3d2dfa87daedca36390
Paul <paulb@bam.tech>
2023-06-30 16:28:12 +0200

    feat: add example file for require-named-effect

---
+++ packages/eslint-plugin/example-app/break-require-named-effect.tsx
@@ -0,0 +1,17 @@
+// This should trigger an error breaking eslint-plugin-bam-custom-rules:
+// bam-custom-rules/require-named-effect
+
+import { useEffect } from "react";
+
+const MyComponent = () => {
+  const doAThing = (t: number) => t;
+
+  useEffect(() => {
+    const abc = 1;
+    doAThing(abc);
+  }, []);
+
+  return <></>;
+};
+
+export default MyComponent;
