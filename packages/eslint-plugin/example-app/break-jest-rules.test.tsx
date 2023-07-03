b3b5b617e48750aa13a9d33074314607fd7f23b3
Paul <paulb@bam.tech>
2023-07-03 15:27:29 +0200

    feat: add examples breaking tests rules

---
+++ packages/eslint-plugin/example-app/break-jest-rules.test.tsx
@@ -0,0 +1,11 @@
+// This should trigger an error breaking eslint-jest rules:
+// jest/valid-title
+// jest/no-identical-title
+
+it("should not fail", () => {
+  expect(1).toBe(1);
+});
+
+it("should not fail", () => {
+  expect(1).toBe(1);
+});
