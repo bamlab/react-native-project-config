4f4419bbef70aca19fd43764d2d7bfaea6a51448
Paul <paulb@bam.tech>
2023-07-04 10:59:47 +0200

    refactor: create configs directory & add reco & tests

--- packages/eslint-plugin/lib/index.js
+++ packages/eslint-plugin/lib/index.js
@@ -1,8 +1,9 @@
 "use strict";
 
 const requireIndex = require("requireindex");
-const recommended = require("./recommended");
-const tests = require("./tests");
+
+const recommended = require("./configs/recommended");
+const tests = require("./configs/tests");
 
 module.exports = {
   configs: {
