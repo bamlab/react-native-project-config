813909f8d3f65436d83d1f98673bf6457b894501
Paul <paulb@bam.tech>
2023-08-01 18:02:50 +0200

    feat: add quick-fix suggestion

--- example-app/eslint-breaking-examples/break-image-requires-accessible-prop.tsx
+++ example-app/eslint-breaking-examples/break-image-requires-accessible-prop.tsx
@@ -1,18 +1,22 @@
-import { Image } from "react-native";
+// Save without formatting: [⌘ + K] > [S]
+
+// This should trigger an error breaking eslint-plugin-bam-custom-rules:
+// bam-custom-rules/image-requires-accessible-prop
+
+import { Image, View } from "react-native";
 
 const MyComponent = () => {
   return (
     <>
-      <Image source={{ uri: "" }} accessible accessibilityIgnoresInvertColors />
-      <Image source={{ uri: "" }} accessible accessibilityIgnoresInvertColors />
+      <Image source={{ uri: "" }} accessibilityIgnoresInvertColors />
+      <Image source={{ uri: "" }} accessibilityIgnoresInvertColors />
       <Image source={{ uri: "random" }} accessibilityIgnoresInvertColors />
       <OtherImage source={{ uri: "random" }} accessibilityIgnoresInvertColors />
-      <OtherImage source={{ uri: "random" }} accessibilityIgnoresInvertColors>
-        <></>
-      </OtherImage>
+      <NotReallyAnImage></NotReallyAnImage>
     </>
   );
 };
 
 const OtherImage = Image;
+const NotReallyAnImage = View;
 export default MyComponent;
--- packages/eslint-plugin/lib/rules/image-requires-accessible-prop.js
+++ packages/eslint-plugin/lib/rules/image-requires-accessible-prop.js
@@ -11,22 +11,25 @@
 /** @type {import('eslint').Rule.RuleModule} */
 module.exports = {
   meta: {
+    hasSuggestions: true,
     type: "problem", // `problem`, `suggestion`, or `layout`
     docs: {
       description: "Require accessible prop on image components",
       recommended: false,
       url: "https://github.com/bamlab/react-native-project-config/tree/main/packages/eslint-plugin/docs/rules/image-requires-accessible-prop.md", // URL to the documentation page for this rule
     },
-    fixable: null, // Or `code` or `whitespace`
+    fixable: "code", // Or `code` or `whitespace`
     schema: [], // Add a schema if the rule has options
     messages: {
       imageRequiresAccessibleProp: "Image components require accessible prop",
+      suggestAddingAccessibleProp: "Add accessible prop",
     },
   },
 
   create(context) {
     return {
       JSXOpeningElement: (node) => {
+        console.log(node);
         if (
           isImage(node) &&
           !node.attributes.some((attr) => attr.name.name === "accessible")
@@ -34,6 +37,18 @@ module.exports = {
           context.report({
             node,
             messageId: "imageRequiresAccessibleProp",
+            suggest: [
+              {
+                messageId: "suggestAddingAccessibleProp",
+                fix: (fixer) => {
+                  const openingTagEnd = node.range[1];
+                  return fixer.insertTextBeforeRange(
+                    [openingTagEnd - (node.selfClosing ? 2 : 1), openingTagEnd],
+                    " accessible={true}"
+                  );
+                },
+              },
+            ],
           });
         }
       },
