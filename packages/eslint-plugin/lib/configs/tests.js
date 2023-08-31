cbd400747c44534c13eeea5e5761cdd01a8aa0fe
Pierre Zimmermann <64224599+pierrezimmermannbam@users.noreply.github.com>
2023-08-31 14:36:54 +0200

    BREAKING CHANGE: upgrade testing library plugin (#51)
    * chore: add all peer dependencies as dev dependencies
    
    * BREAKING CHANGE(eslint-plugin): update testing library plugin to 6.0.1
    
    ---------
    
    Co-authored-by: pierrezimmermann <pierrez@nam.tech>

--- packages/eslint-plugin/lib/configs/tests.js
+++ packages/eslint-plugin/lib/configs/tests.js
@@ -30,15 +30,15 @@ module.exports = defineConfig({
     "jest/valid-expect-in-promise": "error",
     "jest/valid-title": ["error", { disallowedWords: ["should"] }], // STYLE
     "jest-formatting/padding-around-all": "error", // STYLE
-    "testing-library/await-async-query": "error",
+    "testing-library/await-async-queries": "error",
     "testing-library/no-manual-cleanup": "error",
     "testing-library/no-container": "error",
-    "testing-library/no-await-sync-query": "error",
+    "testing-library/no-await-sync-queries": "error",
     "testing-library/no-await-sync-events": "error",
     "testing-library/no-debugging-utils": "error",
     "testing-library/await-async-utils": "error",
     "testing-library/no-promise-in-fire-event": "error",
-    "testing-library/no-render-in-setup": "error",
+    "testing-library/no-render-in-lifecycle": "error",
     "testing-library/no-unnecessary-act": "error",
     "testing-library/no-wait-for-multiple-assertions": "error",
     "testing-library/prefer-explicit-assert": "error",
