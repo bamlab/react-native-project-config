fbdcd154b28761d32225469fa2ad243eb12d88a4
Paul Briand <57703518+P0labrD@users.noreply.github.com>
2023-09-14 12:14:00 +0200

    feat: add unused imports rules (#57)
    * feat: replace typescript no-unused-vars with unused-imports/no-unused-vars
    
    * feat: replace break-typescript-eslint-rules with break-unused-imports-rules

--- example-app/eslint-breaking-examples/break-typescript-eslint-rules.ts
+++
@@ -1,7 +0,0 @@
-// Save without formatting: [⌘ + K] > [S]
-
-// This should trigger two errors breaking typescript-eslint rules:
-// @typescript-eslint/no-unused-vars
-// @typescript-eslint/ban-types
-
-const str: String = "foo";
---
+++ example-app/eslint-breaking-examples/break-unused-imports-rules.tsx
@@ -0,0 +1,9 @@
+// Save without formatting: [⌘ + K] > [S]
+
+// This should trigger two errors breaking unused-imports rules:
+// unused-imports/no-unused-imports
+// unused-imports/no-unused-vars
+
+import { View } from "react-native";
+
+const unused_var = 1;
