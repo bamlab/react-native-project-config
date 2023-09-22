a3611c8c4dfada9974033d9597ee33c873a98c78
Paul <paulb@bam.tech>
2023-09-22 14:44:29 +0200

    BREAKING CHANGE: remove custom rules from config a11y

--- packages/eslint-plugin/lib/configs/a11y.js
+++ packages/eslint-plugin/lib/configs/a11y.js
@@ -7,10 +7,5 @@ module.exports = defineConfig({
   extends: ["plugin:react-native-a11y/all"],
   rules: {
     "react-native-a11y/has-accessibility-hint": "off",
-    "@bam.tech/image-requires-accessible-prop": "error",
-    "@bam.tech/do-not-use-role-on-image": "error",
-    "@bam.tech/accessibility-props-require-accessible": "error",
-    "@bam.tech/requires-accessibility-role-when-accessible": "error",
-    "@bam.tech/requires-accessibility-label": "error",
   },
 });
