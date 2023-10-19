0736815eaf8bf494aabd28473bed907e98e85b72
Thomas Renaud <thomasre@bam.tech>
2023-10-19 17:46:56 +0200

    feat: Add example for no-color-literals rule

--- example-app/eslint-breaking-examples/not-breaking-rules.tsx
+++ example-app/eslint-breaking-examples/not-breaking-rules.tsx
@@ -1,3 +1,5 @@
+import { StyleSheet } from "react-native";
+
 // This should not trigger a warning nor an error because explicit-function-return-type rule is off
 const noReturnTypeDefined = () => {
   return;
@@ -7,3 +9,11 @@ noReturnTypeDefined();
 // These should not trigger a warning nor an error because no-console rule is off for warns and errors
 console.warn("This is a warning message");
 console.error("This is an error message");
+
+// This should not trigger a warning nor an error because no-color-literals rule is off
+const styles = StyleSheet.create({
+  view: {
+    color: "red",
+  },
+});
+styles.view;
