b29f3d2d2e9b50d944dc4160a114edd219d9fdee
Fanny Tavart <fannyt@bam.tech>
2024-08-13 15:51:54 +0200

    refactor(a11y.ts): fix camel case typo in config name

--- packages/eslint-plugin/lib/configs/a11y.ts
+++ packages/eslint-plugin/lib/configs/a11y.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "eslint-define-config";
 
-export const a11yconfig = defineConfig({
+export const a11yConfig = defineConfig({
   extends: ["plugin:react-native-a11y/all"],
   rules: {
     "react-native-a11y/has-accessibility-hint": "off",
