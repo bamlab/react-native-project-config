00d8b523f066437be7d50d45a81a4a6fe375d8b9
Fanny Tavart <fannyt@bam.tech>
2024-08-14 10:09:37 +0200

    BREAKING CHANGE: add new file to improve performance with eslint rules

--- packages/eslint-plugin/lib/configs/index.ts
+++ packages/eslint-plugin/lib/configs/index.ts
@@ -2,10 +2,12 @@ import { a11yConfig } from "./a11y";
 import { importConfig } from "./import";
 import { recommendedConfig } from "./recommended";
 import { testsConfig } from "./tests";
+import { performanceConfig } from "./performance";
 
 export default {
   recommended: recommendedConfig,
   tests: testsConfig,
   a11y: a11yConfig,
   import: importConfig,
+  performance: performanceConfig,
 };
